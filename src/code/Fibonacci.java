33bb14b47371d0aa197f3d445b5ef585d427a7f9
luroma0511-SSC <luroma0511@live.seminolestate.edu>
2026-05-17 11:25:49 -0400

    Added javadoc comments to the fibonacci method.

--- src/code/Fibonacci.java
+++ src/code/Fibonacci.java
@@ -7,6 +7,15 @@ public class Fibonacci {
 		System.out.println("The " + term + "th term of the Fibonacci sequence is " + result + "."); 
 	}
 
+	/** 
+	 * Calculates the Fibonacci number at the specified position	
+	 * using a recursive algorithm.
+	 * The Fibonacci sequence begins with:
+	 * 0, 1, 1, 2, 3, 5, 8, ...
+	 * 
+	 * @param number the position in the Fibonacci sequence to calculate.
+	 * @return the Fibonacci number at the given position.
+	 */
 	private static int fibonacci(int number){
 		if (number <= 1){
 			return number;
